541e75de5bf5475e9322f6ab0437991b9f1efd9c
Anders Zommarin <az@winmac.lab.zommarin.net>
2010-09-29 11:50:41 +0200

    Some T4 examples

---
+++ T4Test/Template1.cs
@@ -0,0 +1,17 @@
+﻿public class Tables {
+    public string ASP { get; set; }
+    public string FooDb { get; set; }
+    public string master { get; set; }
+    public string model { get; set; }
+    public string msdb { get; set; }
+    public string MyDb { get; set; }
+    public string MyDb_dbssA { get; set; }
+    public string MyDb_dbssB { get; set; }
+    public string OPIXAZ { get; set; }
+    public string OPIXOLD { get; set; }
+    public string OPIXPROD { get; set; }
+    public string TaskDb { get; set; }
+    public string TaskNewDb { get; set; }
+    public string tempdb { get; set; }
+    public string TestDb { get; set; }
+}
